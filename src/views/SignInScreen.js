2bcfaff9bf3f0aef9d8e97021ab7f172bf10b63d
noymashat <34368457+noymashat@users.noreply.github.com>
2020-11-04 18:15:12 +0200

    UI Fix

--- src/views/SignInScreen.js
+++ src/views/SignInScreen.js
@@ -1,16 +1,8 @@
 import React, { Component } from "react";
-import { StyleSheet, Text } from "react-native";
-// import firebase from "firebase";
-import {
-	Container,
-	Content,
-	Form,
-	Item,
-	Input,
-	Button,
-	Label
-} from "native-base";
+import { StyleSheet, View, ImageBackground, Text } from "react-native";
+import { Form, Item, Input, Button, Label } from "native-base";
 import firebase from "../../database/firebase";
+import BG from "../../assets/BG.jpeg";
 
 export default class SignInScreen extends Component {
 	constructor(props) {
@@ -25,7 +17,7 @@ export default class SignInScreen extends Component {
 
 	// keep user signed in
 	componentDidMount() {
-		this.checkIfLoggedIn();
+		// this.checkIfLoggedIn();
 	}
 
 	checkIfLoggedIn = () => {
@@ -59,12 +51,13 @@ export default class SignInScreen extends Component {
 			firebase
 				.auth()
 				.signInWithEmailAndPassword(this.state.email, this.state.password)
-				.then(res => {
+				.then(() => {
 					console.log("User logged-in successfully!");
 					this.setState({
 						isLoading: false,
 						email: "",
-						password: ""
+						password: "",
+						error: ""
 					});
 					this.props.navigation.navigate("Task List");
 				})
@@ -78,48 +71,65 @@ export default class SignInScreen extends Component {
 
 	render() {
 		return (
-			<Container style={styles.container}>
-				<Content>
-					<Form>
-						<Item stackedLabel>
-							<Label>Email</Label>
-							<Input onChangeText={this.handleEmail} />
-						</Item>
-						<Item stackedLabel last>
-							<Label>Password</Label>
-							<Input
-								onChangeText={this.handlePassword}
-								secureTextEntry={true}
-							/>
-						</Item>
-						{this.state.error === "" ? (
-							<Text style={{ fontSize: 14, padding: 10 }}></Text>
-						) : (
-							<Text style={styles.errorText}>Error: {this.state.error}</Text>
-						)}
-						<Button full light onPress={this.onSignIn}>
-							<Text style={{ fontSize: 18 }}> SIGN IN </Text>
-						</Button>
-						<Text style={styles.text}>or</Text>
-						<Button
-							full
-							light
-							onPress={() => this.props.navigation.navigate("Sign Up")}
-						>
-							<Text style={{ fontSize: 18 }}> SIGN UP </Text>
-						</Button>
-					</Form>
-				</Content>
-			</Container>
+			<View style={styles.container}>
+				<ImageBackground style={styles.image} source={BG}>
+					<View style={styles.view1}>
+						<Form style={styles.form}>
+							<Item style={{ ...styles.formItem, margin: 10 }} stackedLabel>
+								<Label>Email</Label>
+								<Input onChangeText={this.handleEmail} />
+							</Item>
+							<Item style={styles.formItem} stackedLabel last>
+								<Label>Password</Label>
+								<Input
+									onChangeText={this.handlePassword}
+									secureTextEntry={true}
+								/>
+							</Item>
+							{this.state.error === "" ? (
+								<Text style={{ fontSize: 14, padding: 10 }}></Text>
+							) : (
+								<Text style={styles.errorText}>Error: {this.state.error}</Text>
+							)}
+							<Button style={styles.button} full light onPress={this.onSignIn}>
+								<Text style={{ fontSize: 18 }}> SIGN IN </Text>
+							</Button>
+							<Text style={styles.text}>or</Text>
+							<Button
+								style={styles.button}
+								full
+								light
+								onPress={() => this.props.navigation.navigate("Sign Up")}
+							>
+								<Text style={{ fontSize: 18 }}> SIGN UP </Text>
+							</Button>
+						</Form>
+						<View style={{ flex: 1.4 }}></View>
+					</View>
+				</ImageBackground>
+			</View>
 		);
 	}
 }
 
 const styles = StyleSheet.create({
+	view1: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		position: "relative",
+		backgroundColor: "rgba(255, 255, 255, 0.3)"
+	},
 	container: {
 		flex: 1,
-		backgroundColor: "#ffffff",
-		justifyContent: "center"
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "white"
+	},
+	image: {
+		height: "100%",
+		width: "100%"
+		// opacity: 0.8
 	},
 	header: {
 		backgroundColor: "#ff6699"
@@ -129,8 +139,37 @@ const styles = StyleSheet.create({
 	},
 	errorText: {
 		textAlign: "center",
-		color: "#FF0000",
-		fontSize: 14,
+		color: "rgb(170, 100, 110)",
+		fontSize: 16,
+		fontWeight: "bold",
 		padding: 10
+	},
+	button: {
+		alignSelf: "center",
+		width: "50%",
+		backgroundColor: "rgba(249, 223, 206, 0.65)",
+		borderRadius: 20,
+		borderColor: "rgb(170, 100, 110)",
+		borderWidth: 2
+	},
+	form: {
+		width: "90%",
+		// height: "45%",
+		// flex: 1,
+		// paddingTop: 50,
+		// justifyContent: "flex-start",
+		backgroundColor: "rgba(255, 255, 255, 0.4)",
+		borderRadius: 10,
+		flex: 1,
+		justifyContent: "flex-start"
+	},
+	formItem: {
+		alignSelf: "center",
+		width: "90%",
+		backgroundColor: "rgba(249, 223, 206, 0.65)",
+		borderRadius: 10,
+		borderColor: "rgb(170, 100, 110)",
+		paddingLeft: 15,
+		borderWidth: 2
 	}
 });
--- src/views/SignUpScreen.js
+++ src/views/SignUpScreen.js
@@ -1,16 +1,8 @@
 import React, { Component } from "react";
-import { StyleSheet, Text } from "react-native";
-// import firebase from "firebase";
-import {
-	Container,
-	Content,
-	Form,
-	Item,
-	Input,
-	Button,
-	Label
-} from "native-base";
+import { StyleSheet, View, ImageBackground, Text } from "react-native";
+import { Form, Item, Input, Button, Label } from "native-base";
 import firebase from "../../database/firebase";
+import BG from "../../assets/BG.jpeg";
 
 export default class SignInScreen extends Component {
 	constructor(props) {
@@ -84,69 +76,116 @@ export default class SignInScreen extends Component {
 
 	render() {
 		return (
-			<Container style={styles.container}>
-				<Content>
-					<Form>
-						<Item stackedLabel>
-							<Label>First Name</Label>
-							<Input onChangeText={this.handleFirstName} />
-						</Item>
-						<Item stackedLabel>
-							<Label>Last Name</Label>
-							<Input onChangeText={this.handleLastName} />
-						</Item>
-						<Item stackedLabel>
-							<Label>Email</Label>
-							<Input onChangeText={this.handleEmail} />
-						</Item>
-						<Item stackedLabel>
-							<Label>Password</Label>
-							<Input
-								onChangeText={this.handlePassword}
-								secureTextEntry={true}
-							/>
-						</Item>
-						<Item stackedLabel last>
-							<Label>Confirm Password</Label>
-							<Input
-								onChangeText={this.handleConfPassword}
-								secureTextEntry={true}
-							/>
-						</Item>
-						{this.state.error === "" ? (
-							<Text style={{ fontSize: 14, padding: 10 }}></Text>
-						) : (
-							<Text
-								style={{
-									textAlign: "center",
-									color: "#FF0000",
-									fontSize: 14,
-									padding: 10
-								}}
-							>
-								Error: {this.state.error}
-							</Text>
-						)}
-						<Button full light onPress={this.onSignUp}>
-							<Text> SIGN UP </Text>
-						</Button>
-					</Form>
-				</Content>
-			</Container>
+			<View style={styles.container}>
+				<ImageBackground style={styles.image} source={BG}>
+					<View style={styles.view1}>
+						<Form style={styles.form}>
+							<Item style={styles.formItem} stackedLabel>
+								<Label>First Name</Label>
+								<Input onChangeText={this.handleFirstName} />
+							</Item>
+							<Item style={styles.formItem} stackedLabel>
+								<Label>Last Name</Label>
+								<Input onChangeText={this.handleLastName} />
+							</Item>
+							<Item style={styles.formItem} stackedLabel>
+								<Label>Email</Label>
+								<Input onChangeText={this.handleEmail} />
+							</Item>
+							<Item style={styles.formItem} stackedLabel>
+								<Label>Password</Label>
+								<Input
+									onChangeText={this.handlePassword}
+									secureTextEntry={true}
+								/>
+							</Item>
+							<Item style={styles.formItemLast} stackedLabel last>
+								<Label>Confirm Password</Label>
+								<Input
+									onChangeText={this.handleConfPassword}
+									secureTextEntry={true}
+								/>
+							</Item>
+							{this.state.error === "" ? (
+								<Text style={{ fontSize: 14, padding: 10 }}></Text>
+							) : (
+								<Text style={styles.errorText}>Error: {this.state.error}</Text>
+							)}
+							<Button style={styles.button} full light onPress={this.onSignUp}>
+								<Text style={{ fontSize: 18 }}> SIGN UP </Text>
+							</Button>
+						</Form>
+						<View style={{ flex: 1.4 }}></View>
+					</View>
+				</ImageBackground>
+			</View>
 		);
 	}
 }
 
 const styles = StyleSheet.create({
+	view1: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		position: "relative",
+		backgroundColor: "rgba(255, 255, 255, 0.3)"
+	},
 	container: {
 		flex: 1,
-		backgroundColor: "#ffffff",
-		justifyContent: "center"
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "white"
 	},
-	header: {
-		backgroundColor: "#ff6699"
+	image: {
+		height: "100%",
+		width: "100%"
+		// opacity: 0.8
 	},
-	text: {
-		textAlign: "center"
+	errorText: {
+		textAlign: "center",
+		color: "rgb(170, 100, 110)",
+		fontSize: 16,
+		fontWeight: "bold",
+		padding: 10
+	},
+	button: {
+		alignSelf: "center",
+		width: "50%",
+		backgroundColor: "rgba(249, 223, 206, 0.65)",
+		borderRadius: 20,
+		borderColor: "#fff",
+		borderWidth: 2
+	},
+	form: {
+		width: "90%",
+		height: "70%",
+		// flex: 1,
+		// paddingTop: 50,
+		// justifyContent: "flex-start",
+		backgroundColor: "rgba(255, 255, 255, 0.4)",
+		borderRadius: 10,
+		// flex: 1,
+		justifyContent: "flex-start"
+	},
+	formItem: {
+		alignSelf: "center",
+		width: "90%",
+		backgroundColor: "rgba(249, 223, 206, 0.65)",
+		borderRadius: 10,
+		borderColor: "rgb(170, 100, 110)",
+		paddingLeft: 15,
+		borderWidth: 2,
+		margin: 10
+	},
+	formItemLast: {
+		alignSelf: "center",
+		width: "90%",
+		backgroundColor: "rgba(249, 223, 206, 0.65)",
+		borderRadius: 10,
+		borderColor: "rgb(170, 100, 110)",
+		paddingLeft: 15,
+		borderWidth: 2,
+		marginTop: 10
 	}
 });
