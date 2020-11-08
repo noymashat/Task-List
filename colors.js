aa755b17b503a47ba6d0383fa7ff38fbc7b30f84
noymashat <34368457+noymashat@users.noreply.github.com>
2020-11-08 11:21:54 +0200

    NavBar Background Image Impl.

--- colors.js
+++ colors.js
@@ -1,8 +1,8 @@
 export default {
 	light: {
-		itemOpacity: "rgba(249, 223, 206, 0.65)",
-		itemNoOpacity: "rgba(249, 223, 206, 1)",
-		borderColor: "rgb(170, 100, 110)",
+		itemOpacity: "rgba(240, 210, 190, 0.65)",
+		itemNoOpacity: "rgba(240, 210, 190, 1)",
+		borderColor: "rgb(170, 100, 110)"
 		// formBackground: "rgba(255, 255, 255, 1)"
 	}
 };
